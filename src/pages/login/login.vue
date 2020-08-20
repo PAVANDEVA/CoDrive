8744a2e97d85ea6fe217a1eddb8fa374b80d2116
PAVANDEVA <pavank.devaguptapu@gmail.com>
2020-08-20 11:11:07 +0530

    Add files via upload

---
+++ src/pages/login/login.vue
@@ -0,0 +1,22 @@
+<template>
+  <view class="container">
+    <text>Login</text>
+    <button title="Go to Dashboard" @press="goToHomeScreen"></button>
+  </view>
+</template>
+
+<script>
+export default {
+  // Declare `navigation` as a prop
+  props: {
+    navigation: {
+      type: Object
+    }
+  },
+  methods: {
+    goToHomeScreen() {
+      this.navigation.navigate("dashboard");
+    }
+  }
+}
+</script>
